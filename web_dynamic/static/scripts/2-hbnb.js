fe05292d1a3a7992fa0539075aec3029e1427f6f
SpStigma <yoann.rivet0@gmail.com>
2024-02-09 17:57:27 +0100

    Attempt of task 3.

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+/* global $ */
+$(document).ready(() => {
+  // Object to store Amenity IDs
+  const amenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    // Taking the ID/name of the closest li element
+    const amenityId = $(this).closest('li').data('id');
+    const amenityName = $(this).closest('li').data('name');
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+
+  $.get('http://localhost:5001/api/v1/status/', data => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
